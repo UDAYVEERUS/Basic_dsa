a0be44f369c0010c47ec9abc0d9326ffde7c3e97
Shophree <udayveerus348567@gmail.com>
2023-05-26 08:54:06 +0530

    print odd numbers from an array

--- Array/Even_elements.js
+++ Array/Even_elements.js
@@ -1,8 +1,13 @@
 // Print even elements from an array
 const array = [22,33,41,65,32,44]
 
-for(var i=0;i<array.length-1;i++){
+for(var i=0;i<array.length;i++){
     if(array[i]%2==0){
-        console.log(array[i])
+        console.log("even numbers are "+array[i])
+    }
+}
+for(var j=0;j<array.length;j++){
+    if(array[j]%2!==0){
+        console.log("Odd numbers are "+array[j])
     }
 }
