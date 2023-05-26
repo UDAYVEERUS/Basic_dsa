0cdb52b0a1295bcd09691242b5d8ecf3b6c8dcb1
Shophree <udayveerus348567@gmail.com>
2023-05-26 07:47:19 +0530

    print traversal array

--- Array/each_element_on_new_line.js
+++ Array/each_element_on_new_line.js
@@ -1,8 +1,20 @@
-function newLine(array){
-    for(var i=0;i<array.length;i++){
-        console.log(array[i])
+// function newLine(array){
+//     for(var i=0;i<array.length;i++){
+//         console.log(array[i])
+//     }
+// }
+// // const n = 5
+// const num = [1,2,3,4,5]
+// console.log(newLine(num))
+
+// print reverse traversal array
+function traversalArray(n , arr){
+    let reverse = []
+    for(var i=arr.length-1;i>=0;i--){
+        reverse = reverse  +arr[i]+ " "
     }
+    return reverse
 }
-// const n = 5
-const num = [1,2,3,4,5]
-console.log(newLine(num))
+var  n = 5;
+var array  = [1,2,3,4,5]
+console.log(traversalArray(n,array))
