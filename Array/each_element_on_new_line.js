ff5043528666f25055240d3f17280c27ae1cef16
Shophree <udayveerus348567@gmail.com>
2023-05-26 07:47:53 +0530

    uncomment each line code

--- Array/each_element_on_new_line.js
+++ Array/each_element_on_new_line.js
@@ -1,13 +1,13 @@
-// function newLine(array){
-//     for(var i=0;i<array.length;i++){
-//         console.log(array[i])
-//     }
-// }
-// // const n = 5
-// const num = [1,2,3,4,5]
-// console.log(newLine(num))
+function newLine(array){
+    for(var i=0;i<array.length;i++){
+        console.log(array[i])
+    }
+}
+// const n = 5
+const num = [1,2,3,4,5]
+console.log(newLine(num))
 
-// print reverse traversal array
+//print reverse traversal array
 function traversalArray(n , arr){
     let reverse = []
     for(var i=arr.length-1;i>=0;i--){
