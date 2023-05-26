5f5680a82084d667c995b7d05b18742c2e89c3c4
Shophree <udayveerus348567@gmail.com>
2023-05-26 09:08:58 +0530

    print all charcaters who present on odd and even indexes

--- string/odd_number_str.js
+++ string/odd_number_str.js
@@ -2,6 +2,13 @@
 const string = "udayveersingh"
 for(var i=0;i<string.length;i++){
     if(i%2==0){
-        console.log(string[i])
+        console.log("Even Indexes string are "+string[i])
+    }
+}
+
+// print all charcaters who present on Even indexes
+for(var j=0;j<string.length;j++){
+    if(j%2!==0){
+        console.log("Odd Indexes string are "+string[j])
     }
 }
