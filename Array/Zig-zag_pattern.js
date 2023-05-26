29135ebbbe843545cef46f191975d0e5e22213e7
Shophree <udayveerus348567@gmail.com>
2023-05-26 12:59:28 +0530

    print zigzag pattern

---
+++ Array/Zig-zag_pattern.js
@@ -0,0 +1,21 @@
+const n = 4
+const m = 5
+const matrix = [[1, 2, 3, 4, 5],
+                [6, 7, 8, 9, 10],
+                [11, 12, 13, 14, 15],
+                [16, 17, 18, 19, 20],
+            ]
+var arr = []
+for(var i=0;i<n;i++){
+    if(i%2==0){
+        for(var j=m-1;j>=0;j--){
+            arr.push(matrix[i][j])
+        }
+    }
+    if(i%2!==0){
+        for(var j=0;j<m;j++){
+            arr.push(matrix[i][j])
+        }
+    }
+}
+console.log(arr.join(' '))
