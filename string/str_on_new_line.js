4ad3eb49319c8e3e0ae94d750b24e270338c9c01
Shophree <udayveerus348567@gmail.com>
2023-05-26 08:58:05 +0530

    print each character on the same line

--- string/str_on_new_line.js
+++ string/str_on_new_line.js
@@ -3,3 +3,11 @@ var str = "udayveer"
 for(var i=0;i<str.length;i++){
     console.log(str[i])
 }
+
+// print each character on the same line
+var string = "udayveersingh"
+var same_line = ""
+for(var i=0;i<string.length;i++){
+    same_line = same_line+string[i]
+}
+console.log(same_line)
