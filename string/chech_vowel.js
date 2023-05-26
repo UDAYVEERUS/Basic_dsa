997d1ac1102b445e5290834d222a1fabc49a9f5e
Shophree <udayveerus348567@gmail.com>
2023-05-26 12:10:37 +0530

    remove unused commemnts

--- string/chech_vowel.js
+++ string/chech_vowel.js
@@ -4,7 +4,6 @@ var count = 0
 for(var i=0;i<str.length;i++){
     if(str[i]=="a"||str[i]=="e" || str[i] == "i" || str[i] == "o" || str[i] == "u"){
         count++
-        // console.log("Vowels are present")
     }
 }
 if(count >=1){
