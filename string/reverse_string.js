ff15d559e4eb615fcc4cdd0e8232ea8f09f9ec0f
Shophree <udayveerus348567@gmail.com>
2023-05-26 09:01:27 +0530

    print all reverse characters on a smae line

--- string/reverse_string.js
+++ string/reverse_string.js
@@ -3,3 +3,9 @@ var string = "udayveersingh"
 for(var i=string.length-1;i>=0;i--){
     console.log(string[i])
 }
+// print reverse all characters on the same line
+var reverse = ""
+for(var j=string.length-1;j>=0;j--){
+    reverse =reverse+string[j]
+}
+console.log(reverse)
