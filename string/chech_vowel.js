0874040a96392dd535e3f92520b8bbf0fa1ac7b7
Shophree <udayveerus348567@gmail.com>
2023-05-26 12:22:39 +0530

    checking vowels or consonents

--- string/chech_vowel.js
+++ string/chech_vowel.js
@@ -1,3 +1,4 @@
+// check vowels are present or not
 const str = "udayveer"
 // const str = "stryhg"
 var count = 0
@@ -11,3 +12,20 @@ if(count >=1){
 }else{
     console.log("vowels are not present")
 }
+
+// check count of vowels or consonents in the string
+
+const string = "udayveersingh"
+var vowels = 0
+var consonents = 0
+var count = 0
+for(var i=0;i<string.length;i++){
+    if(string[i]=="a"||string[i]=="e" || string[i] == "i" || string[i] == "o" || string[i] == "u"){
+        vowels++
+    }
+    else{
+        consonents++
+    }
+}
+var bag = "vowels are "+vowels  +" consonents are  "+ consonents
+console.log(bag)
