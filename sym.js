0db54a42261cb9c57d3fdd806173e9c5612650ce
Shophree <udayveerus348567@gmail.com>
2023-06-02 21:11:55 +0530

    print sum of numbers

---
+++ sym.js
@@ -0,0 +1,8 @@
+var num = [2,4,6,70,20,28,59,29,17,37,888,382882,298349,65382]
+var sum = 0;
+for(var i=0;i<num.length-1;i++){
+    sum = sum+num[i]
+}
+
+// print the sum of numbers
+console.log(sum)
