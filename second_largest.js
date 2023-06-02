7e88dc0d6724c81f681967aee0605c4ab89a5bcc
Shophree <udayveerus348567@gmail.com>
2023-06-02 21:09:57 +0530

    print second largest number

--- second_largest.js
+++ second_largest.js
@@ -5,4 +5,7 @@ var array = [5,3,7,4,1,9,3]
 array.sort((a,b)=>{
     return b - a
 })
-console.log(array.join())
+// console.log(array.join())
+
+// printing second largest number
+console.log(array[1])
