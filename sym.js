5fda7cb25d0c29cffac2e455446e8c3e02e5a74b
Shophree <udayveerus348567@gmail.com>
2023-06-02 21:13:05 +0530

    printing even numbers

--- sym.js
+++ sym.js
@@ -1,7 +1,10 @@
 var num = [2,4,6,70,20,28,59,29,17,37,888,382882,298349,65382]
 var sum = 0;
 for(var i=0;i<num.length-1;i++){
-    sum = sum+num[i]
+    // printing sum of even numbers
+    if(num[i]%2==0){
+        sum = sum+num[i]
+    }
 }
 
 // print the sum of numbers
